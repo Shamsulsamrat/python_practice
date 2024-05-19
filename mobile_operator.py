a49680f70eb7bfcd85c52741362ffffff751565b
Shamsulsamrat <shamsulsamrat1090@gmail.com>
2024-05-19 08:13:18 +0600

    problem solve

---
+++ mobile_operator.py
@@ -0,0 +1,17 @@
+mobile_number=str(input("Enter Your Mobile Number:"))
+if mobile_number.startswith("019"):
+    print("Your Opertor is Banglalink")
+    
+if mobile_number.startswith("017"):
+    print("Your Opertor is GrameenPhone")
+    
+if mobile_number.startswith("016"):
+    print("Your Opertor is Airtel")
+    
+if mobile_number.startswith("018"):
+    print("Your Opertor is Robi")
+
+if mobile_number.startswith("015"):
+    print("Your Opertor is Taletalk")
+
+print(mobile_number)    
