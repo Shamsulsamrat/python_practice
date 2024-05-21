36a13c133908932f3aca396b9212725a440d0df6
Shamsulsamrat <shamsulsamrat1090@gmail.com>
2024-05-21 08:46:36 +0600

    add

---
+++ list (2).py
@@ -0,0 +1,4 @@
+ab=[1,2,3,4,5,6] , target=3
+for i in range(len(ab)):
+  for j in range(i + 1, len(ab)):
+      print(i,j)
---
+++ prblm.py
@@ -0,0 +1,19 @@
+x = range(1,11)
+for n in x: 
+  print(n)
+  
+2,4,6,8,10,12,14,16,18,20
+
+numbers = list(range(2, 22, 2))
+
+
+3,6,9,12,15,18,21,24,27,30
+for num in range(3, 31, 3):  
+  print(num)
+
+# print(numbers)
+
+numbers = [1, 2, 3]
+for num in numbers:
+  for i in range(1, 11):
+    print(num*i)  
