1242e9df34628ffddce127f793ed9bba1074c910
Shamsulsamrat <shamsulsamrat1090@gmail.com>
2024-05-26 21:13:53 +0600

    dict excercise file added

---
+++ dict_excer_.py
@@ -0,0 +1,35 @@
+'''No:01
+Use the get method to print the value of the "model" key of the car dictionary.
+car =	{
+   "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(?)
+'''
+#Ans:
+car =	{
+   "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(car.get("model"))
+
+'''No:02
+What will be the output of the following code?
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+           x = car.items()
+'''
+#Ans
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+x = car.items()
+print(x)
+
