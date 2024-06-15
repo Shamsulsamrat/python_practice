e4076ac2ea0ad2223caaeb0957c45ae2b70052bb
Shamsulsamrat <shamsulsamrat1090@gmail.com>
2024-06-15 10:28:17 +0600

    File Added

---
+++ v.py
@@ -0,0 +1,49 @@
+# s="I love you"
+# z=s.lower()
+# print(z)
+
+# x=("a","e","I","o","u")
+# for d in x:
+#     z.count(d)
+#     print(d,z.count(d))
+
+
+# a = [1, 2, 3, 8,5]
+# b = [3, 6, 9, 10,12,1]
+# c = []
+# for i in a:
+#   for j in b:
+#     c.append(i * j)
+
+# print(c)
+# x=set(c)
+# for d in x:
+#     print(d,c.count(d))
+# print(x)
+
+
+# x=int(input("enter any number:"))  
+# for z in range(1,x+1):
+#         print("*"*z)
+
+
+# x=range(0,20+1,2)
+# for z in x:
+#         print(z)
+        
+        
+# z="My name is Rasel"
+# print(z.capitalize())
+# print(z.casefold())
+# print(z.upper())
+# print("rasel" in z.capitalize()) 
+
+# x=int(input("Enter Your Year:"))
+# if x%2==0:
+#         print("It's Leap year")
+# elif x%4==0 :  
+#   print("It's Leap year")
+# else:
+#  print("It's not Leap year ")
+
+  
