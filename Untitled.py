c0b86a97d68479a71356d5cf9a843d6feb3cf130
Shamsulsamrat <shamsulsamrat1090@gmail.com>
2024-06-18 10:55:58 +0600

    file added

---
+++ Untitled.py
@@ -0,0 +1,21 @@
+marks=float(input("Enter Your Marks:"))
+
+if marks >79 and marks <=100:
+    print("A+")
+    
+elif marks >69 and marks <=80:
+    print("A")
+    
+elif marks >59 and marks <=70:
+    print("A-")
+    
+elif marks >49 and marks <=60:
+    print("B")
+    
+elif marks >39 and marks <=50:
+    print("C")
+    
+elif marks >32 and marks <=40:
+    print("D")
+else:
+    print("Fail")
